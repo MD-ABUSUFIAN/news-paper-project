2978554bf15fa5f37f2394fd0cc6d817936f108a
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2021-09-29 18:58:01 +0600

    react and bootstrap news paper project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Row, Spinner } from 'react-bootstrap';
+import News from './Component/News/News';
+import Header from './Component/Header/Header';
 
 function App() {
+  
+  const[news,setNews]=useState([]);
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-29&sortBy=publishedAt&apiKey=28ecbd2a44fa4cef8ce67328c1503b8f')
+    .then(res=>res.json())
+    .then(data=>setNews(data.articles))
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Header></Header>
+  {
+    news.length===0 ?<Spinner animation="border" variant="primary" />:<Row xs={1} md={3} lg={4} className="g-4">
+    {news.map(nw=><News News={nw}></News>)}
+  </Row>
+  }
+
+</div>
   );
-}
+};
 
 export default App;
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Nasrin News Paper</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features">Top News</Nav.Link>
+      <Nav.Link href="#pricing">Today News</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Update News</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Old News</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Old News</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Header;
